4c45604f6a05c439f524bd63988a9150249c5409
Johnsonlee <johnsonlee28@gmail.com>
2017-04-18 09:07:34 +0800

    job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,54 @@
+class JobsController < ApplicationController
+
+  before_filter :authenticate_user!, only:[:new, :create, :update, :edit, :destroy]
+  
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "Job Created!"
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      flash[:notice] = "Job Updated!"
+      redirect_to job_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.delete
+    flash[:warning] = "Job Deleted!"
+    redirect_to job_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Hello World!'
+puts '這個種子檔會自動建立一個帳號, 並且随机創建 10 個jobs，10个隐藏的jobs'
+
+create_account = User.create([email: '1@1.com', password: '123123', password_confirmation: '123123'])
+puts 'Admin account is created successfully!'
+
+job_info = [
+  '招聘RoR工程师',
+  '招聘文案设计',
+  '招聘UI设计师',
+  '招聘Android开发工程师',
+  '招聘产品经理',
+  '招聘前端开发工程师',
+  '招聘市场营销',
+  '招聘php后台研发工程师',
+  '招聘python工程师',
+  '招聘高级JAVA研发工程师',
+  '招聘高级数据挖掘工程师',
+  '招聘高级客服经理',
+  '招聘市场总监',
+]
+
+job_city = [
+  '广州',
+  '深圳',
+  '天津',
+  '北京',
+  '珠海',
+  '上海',
+  '哈尔滨',
+  '辽宁',
+]
+
+job_company = [
+  '阿里巴巴',
+  '人人网',
+  '汇添宝',
+  '广晟集团',
+  '威威集团',
+  '李白集团',
+  '巴塔木广告有限公司',
+  '酷派家居有限公司',
+  '利天',
+  '登峰极集团',
+  '盛天庄集团有限公司',
+  '斯柏菲',
+]
+
+job_email = [
+  'johnsonlee@126.com',
+  'example@163.com',
+  'xxoxx@gmail.com',
+  'coco@163.com'
+]
+
+create_jobs = for i in 1..10 do
+                Job.create!([title: job_info[rand(job_info.length)], description: "这是一个公开的工作"])
+              end
+for i in 1..10 do
+  Job.create!([title: job_info[rand(job_info.length)], description: "这是一个隐藏的工作"])
+end
+
+puts '10 Public jobs created.'
+puts '10 Hidden jobs created.'
