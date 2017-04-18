542d01648da662b41cacc675da0d163d03a10709
Johnsonlee <johnsonlee28@gmail.com>
2017-04-18 10:46:34 +0800

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "你没有权力进入"
+      redirect_to root_path
+    end
+  end
 end
